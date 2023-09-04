fb805cb0260ee6fcfc6b15ccd1c41a8ef8611068
pheerawadee <pheerawadee_c@cmu.ac.th>
2023-09-04 18:50:29 +0700

    done
    done

--- components/donation.tsx
+++ components/donation.tsx
@@ -2,8 +2,35 @@
 // import { type Donation } from "@/utils/types";
 import { Paper, Text, Stack, Group, Title, Card } from "@mantine/core";
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+
+interface DonationInterface {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  amount: number;
+  time: string;
+}
 
 export default function Donation() {
+  const [list, setList] = useState<DonationInterface[]>([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://donation-server-production.up.railway.app/donation")
+        .then((res) => res.json())
+        .then((res) => setList(res));
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://donation-server-production.up.railway.app/donation")
+      .then((res) => res.json())
+      .then((res) => setList(res));
+  }, []);
+
   return (
     <Card withBorder shadow="xs" bg="gray.3">
       <Group mb={20}>
@@ -11,40 +38,28 @@ export default function Donation() {
           Total
         </Title>
         <Title order={1} variant="gradient">
-          10000
+          {list
+            .reduce((partialSum, a) => partialSum + a.amount, 0)
+            .toLocaleString("en-US")}
         </Title>
         <Title order={1} color="gray">
           THB
         </Title>
       </Group>
       <Stack>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
-        <Paper shadow="xs" p="md">
-          <Group>
-            <Text>Tom</Text>
-            <Text>Sawyer</Text>
-            <Text>tom_sawyer@gmail.com</Text>
-            <Text>10000</Text>
-            <Text>{dayjs("2023-08-26 06:17:51").format("D-MMM HH:mm:ss")}</Text>
-          </Group>
-        </Paper>
+        {list.map((doc) => {
+          return (
+            <Paper key={doc.id} shadow="xs" p="md">
+              <Group>
+                <Text>{doc.firstName}</Text>
+                <Text>{doc.lastName}</Text>
+                <Text>{doc.email}</Text>
+                <Text>{doc.amount.toLocaleString("en-US")}</Text>
+                <Text>{dayjs(doc.time).format("D-MMM HH:mm:ss")}</Text>
+              </Group>
+            </Paper>
+          );
+        })}
       </Stack>
     </Card>
   );
--- components/form.tsx
+++ components/form.tsx
@@ -1,39 +1,171 @@
-// import { API_URL } from "@/utils/api";
 import { Input, Button, Card, Title, Stack } from "@mantine/core";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+const initForm = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  amount: "",
+};
 
 export default function Form() {
+  const [form, setForm] = useState(initForm);
+
+  const [error, setError] = useState(initForm);
+
+  const [loading, setLoading] = useState(false);
+
+  function onChange(event: React.FormEvent<EventTarget>) {
+    const target = event.target as HTMLInputElement;
+    setForm((state) => {
+      return {
+        ...state,
+        [target.name]: target.value,
+      };
+    });
+  }
+
+  function validate() {
+    var e = { ...initForm };
+
+    if (form.firstName == "") {
+      e.firstName = "firstname required";
+    } else {
+      e.firstName = "";
+    }
+
+    if (form.lastName == "") {
+      e.lastName = "lastName required";
+    } else {
+      e.lastName = "";
+    }
+
+    if (form.email == "") {
+      e.email = "email required";
+    }
+
+    if (form.email != "") {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailRegex.test(form.email)) {
+        e.email = "email badly format";
+      } else {
+        e.email = "";
+      }
+    }
+
+    if (form.amount == "") {
+      e.amount = "donation required";
+    }
+
+    if (form.amount != "") {
+      const donationRegex = /^[1-9]\d*$/;
+
+      if (!donationRegex.test(form.amount)) {
+        e.amount = "donation badly format";
+      } else {
+        if (parseInt(form.amount) > 1000) {
+          e.amount = "";
+        } else {
+          e.amount = "Amount must be greater than 1000";
+        }
+      }
+    }
+
+    setError(e);
+
+    if (
+      e.firstName == "" &&
+      e.lastName == "" &&
+      e.email == "" &&
+      e.amount == ""
+    )
+      return true;
+    else return false;
+  }
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    if (validate() && !loading) {
+      setLoading(true);
+      var response = await fetch(
+        "https://donation-server-production.up.railway.app/donate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...form }),
+        }
+      );
+
+      var json = await response.json();
+      console.log(json);
+      if (json.status === "success") {
+        toast.success("save!");
+        setForm(initForm);
+      } else {
+        toast.error("internal server error");
+      }
+    }
+
+    setLoading(false);
+  }
+
   return (
     <Card withBorder shadow="xs" p="xl" bg="cyan.2">
       <Title order={1} color="blue">
         Donate
       </Title>
+      <Toaster />
 
-      <form>
+      {/* <span>{JSON.stringify(form)}</span> */}
+      <form onSubmit={onSubmit}>
         <Stack spacing={"xs"}>
           <Input.Wrapper>
             <Input.Label>First Name</Input.Label>
-            <Input />
-            <Input.Error>{/* Error goes here */}</Input.Error>
+            <Input
+              required
+              name="firstName"
+              onChange={onChange}
+              value={form.firstName}
+            />
+            <Input.Error>{error.firstName}</Input.Error>
           </Input.Wrapper>
 
           <Input.Wrapper>
             <Input.Label>Last Name</Input.Label>
-            <Input />
-            <Input.Error>{/* Error goes here */}</Input.Error>
+            <Input
+              required
+              name="lastName"
+              onChange={onChange}
+              value={form.lastName}
+            />
+            <Input.Error>{error.lastName}</Input.Error>
           </Input.Wrapper>
 
           <Input.Wrapper>
             <Input.Label>Email</Input.Label>
-            <Input />
-            <Input.Error>{/* Error goes here */}</Input.Error>
+            <Input
+              required
+              name="email"
+              onChange={onChange}
+              value={form.email}
+            />
+            <Input.Error>{error.email}</Input.Error>
           </Input.Wrapper>
 
           <Input.Wrapper>
             <Input.Label>Donation Amount</Input.Label>
-            <Input />
-            <Input.Error>{/* Error goes here */}</Input.Error>
+            <Input
+              required
+              name="amount"
+              onChange={onChange}
+              value={form.amount}
+            />
+            <Input.Error>{error.amount}</Input.Error>
           </Input.Wrapper>
-          <Button>Submit</Button>
+          <Button type="submit">{loading ? "submitting ..." : "Submit"}</Button>
         </Stack>
       </form>
     </Card>
